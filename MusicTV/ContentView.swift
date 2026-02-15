00600c03a9306190d6162a5d852b61bc51daede0
snowmonkey51 <snowmonkey5@gmail.com>
2026-02-15 12:25:46 -0600

    Initial Commit

---
+++ MusicTV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicTV
+//
+//  Created by aaron bevill on 2/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicTV/MusicTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTVApp.swift
+//  MusicTV
+//
+//  Created by aaron bevill on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
